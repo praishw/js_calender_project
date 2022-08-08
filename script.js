4fc7e4191c4223c65cf2bf17875b21aa040ce98a
Aish <aishwaryaghuguskar97@gmail.com>
2022-08-08 09:24:31 +0530

    Commit message

---
+++ script.js
@@ -0,0 +1,60 @@
+var date= new Date();
+function renderDate(){
+  
+
+date.setDate(1);
+
+var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+
+var full_date =days[ date.getDay() ]+'-'+(date.getMonth()+1)+'-'+ date.getFullYear();
+
+var day= date.getDay();
+
+var endDate= new Date(
+  date.getFullYear(),
+  date.getMonth()+1,
+  0
+).getDate();
+
+var prevdate=new Date(
+  date.getFullYear(),
+  date.getMonth(),
+  0
+).getDate();
+
+var today= new Date();
+
+
+var months= ['January', 'February', 'March', 'April', 'May', 'June',
+     'July', 'August', 'September', 'October', 'November', 'December'];
+document.getElementById("full_date").innerHTML=full_date;
+
+document.getElementById("month").innerHTML= months[date.getMonth()];
+
+var cells="";
+
+for(x= day; x>0; x--){
+  cells+= "<div class='pre-data'>"+ (prevdate - x + 1) +"</div> ";
+}
+
+for(i=1; i<=endDate; i++){
+  if(i==today.getDate() && date.getMonth()== today.getMonth()){
+  cells+= "<div class='today'>"+ i +"</div> ";
+  }else{
+    cells+= "<div>"+ i +"</div> ";
+  }
+}
+
+document.getElementsByClassName("dates")[0].innerHTML= cells;
+}
+function moveDate(args){
+  if(args =='prev'){
+    date.setMonth(date.getMonth()-1);
+    renderDate();
+  }else if(args == 'next'){
+    date.setMonth(date.getMonth()+1)
+    renderDate();
+  }
+}
+
+
